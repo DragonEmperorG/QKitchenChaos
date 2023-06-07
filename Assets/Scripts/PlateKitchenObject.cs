ddaeadffe67daadf429d10a3f582da8aabaab264
lqian@D413 <lqian1995@qq.com>
2023-06-07 19:40:27 +0800

    feat: 完成Plate World UI Icons章节

--- Assets/Scripts/PlateKitchenObject.cs
+++ Assets/Scripts/PlateKitchenObject.cs
@@ -1,8 +1,14 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class PlateKitchenObject : KitchenObject {
+
+    public event EventHandler<OnIngredientAddedEventArgs> OnIngredientAdded;
+    public class OnIngredientAddedEventArgs : EventArgs { 
+        public KitchenObjectSO kitchenObjectSO;
+    }
 
     [SerializeField] private List<KitchenObjectSO> validKitchenObjectSOList;
 
@@ -21,8 +27,17 @@ public class PlateKitchenObject : KitchenObject {
             return false;
         } else {
             kitchenObjectSOList.Add(kitchenObjectSO);
+
+            OnIngredientAdded?.Invoke(this, new OnIngredientAddedEventArgs { 
+                kitchenObjectSO = kitchenObjectSO
+            });
+
             return true;
         }
+    }
+
+    public List<KitchenObjectSO> GetKitchenObjectSOList() { 
+        return kitchenObjectSOList;
     }
 
 }
