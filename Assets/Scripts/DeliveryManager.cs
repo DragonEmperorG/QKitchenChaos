091ec1d79bddc4ddaa02f99a917d90367e450104
lqian@D413 <lqian1995@qq.com>
2023-06-15 23:32:57 +0800

    feat: 完成Main Menu, Loading章节

--- Assets/Scripts/DeliveryManager.cs
+++ Assets/Scripts/DeliveryManager.cs
@@ -18,6 +18,7 @@ public class DeliveryManager : MonoBehaviour {
     private float spawnRecipeTiimer;
     private float spawnRecipeTimerMax = 4f;
     private int waitingRecipesMax = 4;
+    private int successfulRecipesAmount;
 
     private void Awake() {
         Instance = this;
@@ -66,6 +67,7 @@ public class DeliveryManager : MonoBehaviour {
                 }
 
                 if (plateContentsMatchesRecipe) {
+                    successfulRecipesAmount++;
                     waitingRecipeSOList.RemoveAt(i);
 
                     OnRecipeCompleted?.Invoke(this, EventArgs.Empty);
@@ -83,6 +85,10 @@ public class DeliveryManager : MonoBehaviour {
 
     public List<RecipeSO> GetWaitingRecipeSOList() { 
         return waitingRecipeSOList;
+    }
+
+    public int GetSuccessfulRecipesAmount () { 
+        return successfulRecipesAmount; 
     }
 
 }
--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -33,16 +33,23 @@ public class Player : MonoBehaviour, IKitchenObjectParent {
     private void Start() {
         gameInput.OnInteractAction += GameInput_OnInteractAction;
         gameInput.OnInteractAlternateAction += GameInput_OnInteractAlternateAction;
-        ;
     }
 
     private void GameInput_OnInteractAlternateAction(object sender, EventArgs e) {
+        if (!KitchenGameManager.Instance.IsGamePlaying()) {
+            return;
+        }
+
         if (selectedCounter != null) {
             selectedCounter.InteractAlternate(this);
         }
     }
 
     private void GameInput_OnInteractAction(object sender, System.EventArgs e) {
+        if (!KitchenGameManager.Instance.IsGamePlaying()) {
+            return;
+        }
+
         if (selectedCounter != null) {
             selectedCounter.Interact(this);
         }
